6943c494258f0f5b739d276c2a782412bc30dc29
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-09-02 18:17:37 -0400

    Add IBOutlets & IBAction for buttons and label.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stroyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
